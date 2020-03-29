e7c7217ee0ddd3c0d9f9d5e6eef143f716b64551
nzmonzmp <3600236+nzmonzmp@users.noreply.github.com>
2020-03-29 22:12:05 +0200

    maj orsys/mlb +/ template/orsys/ + nettoyage
    Former-commit-id: 5c0aff99b719aebc812066e865bf786ecc3e9792 [formerly f894905a6ded14c538d8886d9537ac8c64448034]
    Former-commit-id: 47aa5f1940c430f40da3834eb69c823e561fbf63

--- orsys/dpl/1/clean-tex.sh
+++ orsys/dpl/1/clean-tex.sh
@@ -5,3 +5,4 @@ rm *~
 a=$(ls *tex)
 b=$(grep "[^*%]\.pdf" Makefile | sed "s/[ \][ \]*$//g;s/^.*  *//g" | grep -v "(" | grep -v "cours" | sed "s/\.pdf/.tex/g" | sort | uniq)
 rm $(echo -e "$a\n$b" | sort | uniq -ic | sort -n | grep -v " 2 " | sed "s/^.* //g")
+echo -e "$a\n$b" | sort | uniq -ic | sort -n | grep -v " 2 " | sed "s/^.* //g"
