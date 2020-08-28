3c002516d3c46d5085e485a862ebaac0cadfca17
nzmonzmp <36000236+nzmonzmp@users.noreply.github.com>
2020-08-28 17:50:34 +0200

    nlp minor changes

--- code-illustration/regex/demo-str-lit-brut.py
+++ code-illustration/regex/demo-str-lit-brut.py
@@ -2,5 +2,5 @@ string_lit  =  "hello\nworld"
 string_brut = r"hello\nworld"
 print("Chaine littérale : ")
 print(string_lit)
-print("Chaine littérale : ")
+print("Chaine brute : ")
 print(string_brut)
