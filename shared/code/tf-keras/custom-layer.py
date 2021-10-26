fed6ca78ed0a9ebf46827d36f944feb1148b7194
m09 <142691+m09@users.noreply.github.com>
2021-10-26 23:23:19 +0200

    Fix DPL as agreed during review
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- shared/code/tf-keras/custom-layer.py
+++ shared/code/tf-keras/custom-layer.py
@@ -1,21 +1,18 @@
 class MyLayer(tf.keras.layers.Layer):
+    # Sauvegarde des paramètres
     def __init__(self, output_dim, **kwargs):
         super().__init__(**kwargs)
         self.output_dim = output_dim
 
+    # Création des poids
     def build(self, input_shape):
         self.kernel = self.add_weight(
-            name='kernel', shape=(input_shape[1], self.output_dim),
-            initializer='uniform', trainable=True)
+            name="kernel",
+            shape=(input_shape[1], self.output_dim),
+            initializer="uniform",
+            trainable=True,
+        )
 
+    # Calcul de la couche
     def call(self, inputs):
         return inputs @ self.kernel
-
-    def get_config(self):
-        base_config = super().get_config()
-        base_config['output_dim'] = self.output_dim
-        return base_config
-
-    @classmethod
-    def from_config(cls, config):
-        return cls(**config)
